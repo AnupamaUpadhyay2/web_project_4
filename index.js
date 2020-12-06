a8d72b5844d08af30106e84e9555bd47d15ac426
Anupama Upadhyay <anupama.upadhyay2@gmail.com>
2020-12-06 12:53:12 +0100

    version 1

---
+++ index.js
@@ -0,0 +1,30 @@
+let editButton = document.querySelector(".profile__edit");
+let closeButton = document.querySelector(".modal__button");
+let form = document.querySelector(".form");
+let modal = document.querySelector(".modal");
+
+let nameInput = document.querySelector(".form__input_type_name");
+let aboutMeInput = document.querySelector(".form__input_type_about-me");
+
+let profileName = document.querySelector(".profile__name");
+let profileAbout = document.querySelector(".profile__about");
+
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
+
+  profileName.textContent = nameInput.value;
+  profileAbout.textContent = aboutMeInput.value;
+
+  toggleModal()
+
+})
+
+function toggleModal() {
+  modal.classList.toggle("modal_open");
+}
+
+editButton.addEventListener("click", toggleModal)
+
+closeButton.addEventListener("click", toggleModal)
+
