eaec290789745d40f83e6d9c4b72c49bbfa6b1a2
Anupama Upadhyay <anupama.upadhyay2@gmail.com>
2021-01-16 12:28:07 +0100

    Fixes - Add esc key to open modal & disable submit for form reset

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,14 @@
 //wrappers
+const page = document.querySelector(".page");
+const modals = Array.from(document.querySelectorAll(".modal"));
+const modalContainers = Array.from(document.querySelectorAll(".modal__container"));
 const editProfileModal = document.querySelector(".modal_type_edit-profile")
 const addCardModal = document.querySelector(".modal_type_add-card")
 const imageModal = document.querySelector(".modal_type_image")
 
 const editProfileForm = editProfileModal.querySelector(".form");
 const addCardForm = addCardModal.querySelector(".form");
-
+const submitCardButton = addCardForm.querySelector(".form__save");
 //open buttons
 const profileEditButton = document.querySelector(".profile__edit");
 const addCardButton = document.querySelector(".profile__add-button");
@@ -35,12 +38,20 @@ let openedModal;
 
 function openModal(modal) {
   modal.classList.toggle("modal_open");
-  openedModal = modal
+  openedModal = modal;
+  document.addEventListener("keydown", handleEscapeKey);
 }
 
 function closeModal(modal) {
   modal.classList.toggle("modal_open");
-  openedModal = null
+  openedModal = null;
+  document.removeEventListener("keydown", handleEscapeKey);
+}
+
+function handleEscapeKey(event){
+  if (event.key === "Escape") {
+    closeModal(openedModal);
+  }
 }
 
 function valueUpdate(event) {
@@ -61,12 +72,24 @@ function updatePhotoGrid(event) {
   };
 
   addCardToGrid(newCard);
-
+  addCardForm.reset();
+  submitCardButton.disabled = true
+  submitCardButton.classList.add("form__save_disabled");
   closeModal(addCardModal);
 }
 
 
+function addCardToGrid(data) {
+  const cardElement = createCard(data);
+  photoGrid.prepend(cardElement);
+}
+
 addCardForm.addEventListener("submit", updatePhotoGrid);
+
+// addCardForm.addEventListener("submit", () => {
+//   updatePhotoGrid(event);
+//   toggleButton(button, config, inputs);
+// })
 
 profileEditButton.addEventListener("click", () => {
 
@@ -83,7 +106,6 @@ closeEditProfileModal.addEventListener("click", () => {
 editProfileForm.addEventListener("submit", valueUpdate);
 
 addCardButton.addEventListener("click", () => {
-  addCardForm.reset();
   openModal(addCardModal);
 })
 
@@ -92,32 +114,57 @@ closeAddCardModal.addEventListener("click", () => {
 })
 
 
-const initialCards = [
-  {
-    name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
-  },
-  {
-    name: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
-  },
-  {
-    name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
-  },
-  {
-    name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
-  },
-  {
-    name: "Vanoise National Park",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg"
-  }
-];
+// const initialCards = [
+//   {
+//     name: "Yosemite Valley",
+//     link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+//   },
+//   {
+//     name: "Lake Louise",
+//     link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+//   },
+//   {
+//     name: "Bald Mountains",
+//     link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+//   },
+//   {
+//     name: "Latemar",
+//     link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+//   },
+//   {
+//     name: "Vanoise National Park",
+//     link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+//   },
+//   {
+//     name: "Lago di Braies",
+//     link: "https://code.s3.yandex.net/web-code/lago.jpg"
+//   }
+// ];
+
+initialCards.forEach(card => {
+  addCardToGrid(card);
+})
+
+
+closeImageModal.addEventListener("click", () => {
+  closeModal(imageModal);
+})
+
+modals.forEach((modal, index) => {
+
+  modal.addEventListener("click", (evt) => {
+    if(evt.target === modal || evt.target === modalContainers[index]){
+      closeModal(modal)
+    }
+  })
+})
+
+// page.addEventListener("keydown", (evt) => {
+  
+//   if (evt.key === "Escape" && openedModal) {
+//     closeModal(openedModal)
+//   }
+// })
 
 
 function createCard(data) {
@@ -132,9 +179,15 @@ function createCard(data) {
   cardImage.src = data.link;
   cardImage.alt = data.name;
 
-  cardLikeButton.addEventListener("click", () => {
+  function toggleLikeButton(){
     cardLikeButton.classList.toggle("photo-grid__like-button_liked");
-  })
+  }
+
+  cardLikeButton.addEventListener("click", toggleLikeButton);
+  
+  // () => {
+  //   cardLikeButton.classList.toggle("photo-grid__like-button_liked");
+  // })
 
   cardDeleteButton.addEventListener("click", () => {
     cardElement.remove();
@@ -144,52 +197,10 @@ function createCard(data) {
 
     popupImage.src = data.link;
     popupImageCaption.textContent = data.name;
+    popupImage.alt = data.name;
 
     openModal(imageModal);
   })
 
   return cardElement;
 }
-
-
-function addCardToGrid(data) {
-  const cardElement = createCard(data);
-  photoGrid.prepend(cardElement);
-}
-
-
-initialCards.forEach(card => {
-  addCardToGrid(card);
-})
-
-
-closeImageModal.addEventListener("click", () => {
-  closeModal(imageModal);
-})
-
-const modals = Array.from(document.querySelectorAll(".modal"));
-const modalContainers = Array.from(document.querySelectorAll(".modal__container"));
-
-modals.forEach((modal, index) => {
-
-  modal.addEventListener("click", (evt) => {
-    console.log(evt.target)
-    if(evt.target == modal || evt.target == modalContainers[index]){
-      closeModal(modal)
-    }
-  })
-
- 
-})
-
-
-const page = document.querySelector(".page");
-
-page.addEventListener("keydown", (evt) => {
-  
-  if (evt.key === "Escape" && openedModal) {
-    closeModal(openedModal)
-  }
-})
-
-
